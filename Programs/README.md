49490ea64d02cfe6236bf84b8f07a116199ccd60
Abdul Nizam <abdulnizam@freniz.com>
2018-10-02 12:29:06 +0400

    updated the query delete in python

---
+++ Programs/README.md
@@ -0,0 +1,17 @@
+# Programs
+
+	All example python codes will be here to discuss
+
+
+use env to locate all dependency of the pip
+
+To active the environment
+
+    python3 -m venv "env name"
+
+    source "env name"/bin/activate
+
+To deactive the environment
+
+    deactivate
+
---
+++ Programs/query-delete-vmps.py
@@ -0,0 +1,52 @@
+import os
+
+
+from dotenv import load_dotenv, find_dotenv
+load_dotenv(find_dotenv())
+
+import mysql.connector
+import json
+
+mydb = mysql.connector.connect(
+  host=os.getenv('HOST'),
+  user=os.getenv('USERNAME'),
+  passwd=os.getenv('PASSWORD'),
+  database=os.getenv('DATABASE')
+)
+
+json = {
+	"data":
+	[
+		{
+			"transactions_id": "001538148155615",
+			"COUNT(transactions_id)": 2,
+			"created_at": "2018-09-28 19:22:34"
+		},
+		{
+			"transactions_id": "011538066236557",
+			"COUNT(transactions_id)": 2,
+			"created_at": "2018-09-27 20:37:15"
+		},
+		{
+			"transactions_id": "011538225813940",
+			"COUNT(transactions_id)": 2,
+			"created_at": "2018-09-29 16:56:53"
+		}
+	]
+};
+
+leng = len(json['data'])
+
+mycursor = mydb.cursor()
+
+# xrange can be used in python 2 and it deprecated in python 3
+
+for x in range(leng):
+	
+	sql = "DELETE t1 FROM transactions t1 INNER JOIN transactions t2 WHERE t1.id < t2.id AND t1.transactions_id = t2.transactions_id AND t1.transactions_id = '" + json['data'][x]['transactions_id'] + "'";
+
+	mycursor.execute(sql)
+
+	mydb.commit()
+
+	print(mycursor.rowcount, "record(s) deleted")
