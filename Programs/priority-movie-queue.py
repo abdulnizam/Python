c53c1017add2f94da2351d8b4fe4e9c37b751852
Abdulnizam <abdulnizam@freniz.com>
2018-10-04 22:48:34 +0400

    updated the code

--- Programs/priority-movie-queue.py
+++ Programs/priority-movie-queue.py
@@ -4,9 +4,7 @@
 class Solution:
 
 	def __new__(self, nr_movies, priorities, start_times):
-		#
-		# Some work here; return type and arguments should be according to the problem's requirements
-		#
+	
 		movies_list_hour = ""
 		priorities_time_list = {}
 		
@@ -16,62 +14,37 @@ class Solution:
 			priorities_time_list[start_times[i]] = priorities[i]
 		
 		start_times_list.sort()
-
-		sorted_list = [ ]
-
-		# for key in sorted(priorities_time_list.iterkeys()):
-  #   		# print "%s: %s" % (key, mydict[key]) 
-  #   		sorted_list[priorities_time_list[key]] = priorities_time_list[key]
-
-
-		# priorities.sort()
-
-		# return priorities
 		filtered = []
 		unfiltered = []
 		previous_val = 0;
 
 		for x in range(0, nr_movies):
-
-
-			sorted_list.append(start_times_list[x])
-
 			current_val = start_times_list[x]
-			print(previous_val)
 			if current_val != previous_val:
-
 				# future_val = start_times_list[x+(0 if x+1 == nr_movies else 1)]
-
-				# future_val = start_times_list[x+[1, 0][x+1 == nr_movies]]
-
 				future_val = start_times_list[x+[1, 0][x+1 == nr_movies]]
-
-
 				diff = current_val - previous_val
-
 				future_diff = current_val - future_val
-
-				previous_val = current_val
-
 				if diff >= 2:
-					if future_diff >= 2:
+					if previous_val in filtered or future_diff >= 2:
 						filtered.append(current_val)
+						previous_val = current_val
 					else:
 						if priorities_time_list[current_val] > priorities_time_list[future_val]:
-							filtered.append(current_val)
+							if priorities_time_list[current_val] == priorities_time_list[future_val]:
+								filtered.append(future_val)
+							else:
+								filtered.append(current_val)
 						else:
 							filtered.append(future_val)
-
 							previous_val = future_val
 			
-			
 		return filtered
-
-
 
 if __name__ == '__main__':
 
 	solutions = Solution(10,[9,2,10,4,5,1,9,3,6,7],[8,10,11,13,14,15,17,19,20,21])
+	# solutions = Solution(6,[6,2,3,5,2,7],[18,10,12,11,17,13])
 
 	print(solutions)
 
