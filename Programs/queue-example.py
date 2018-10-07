bf7ca794ea86d09f30928b177f9af376139e89c7
Abdul Nizam <abdulnizam@freniz.com>
2018-10-07 10:06:54 +0400

    updated the changes

--- Programs/queue-example.py
+++ Programs/queue-example.py
@@ -9,10 +9,10 @@ queue_list.append("def");
 # full list
 print("Full List : " + str(queue_list))
 # pop left
-queue_list.popleft()
+print("Popped Value Left : " + queue_list.popleft()) # popped value
 # after pop left
 print("After Pop Left (list.popleft()): " + str(queue_list))
 # pop right
-queue_list.pop()
+print("Popped Value Right : " + queue_list.pop()) # popped value
 # after pop right
 print("After Pop Right (list.pop()): " + str(queue_list))
