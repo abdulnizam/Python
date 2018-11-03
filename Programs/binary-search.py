8b053719d7a84082abb350ba8fa0b74420789280
Abdulnizam <abdulnizam@freniz.com>
2018-11-03 21:13:45 +0400

    updated the functions on python program

---
+++ Programs/binary-search.py
@@ -0,0 +1,41 @@
+#!/bin/python
+
+#  method 1 
+def binarySearch(alist, item):
+	first = 0
+	last = len(alist)-1
+	found = False
+	while first<=last and not found:
+		midpoint = (first + last)//2
+		if alist[midpoint] == item:
+			found = True
+		else:
+			if item < alist[midpoint]:
+				last = midpoint-1
+			else:
+				first = midpoint+1
+	return found
+
+def binarySearch2(alist, item):
+	if len(alist) == 0:
+		return False
+	else:
+		midpoint = len(alist)//2
+		if alist[midpoint]==item:
+			return True
+		else:
+			if item<alist[midpoint]:
+				return binarySearch(alist[:midpoint],item)
+			else:
+				return binarySearch(alist[midpoint+1:],item)
+
+
+if __name__ == '__main__':
+	print('method - 1')
+	testlist = [0, 1, 2, 8, 13, 17, 19, 32, 42]
+	print(binarySearch(testlist, 3))
+	print(binarySearch(testlist, 13))
+
+	print('method - 2')
+	print(binarySearch2(testlist, 3))
+	print(binarySearch2(testlist, 13))
